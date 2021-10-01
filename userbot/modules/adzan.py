3d519d10f67c2fcef13a78a2fb2b96ee1b02b690
mrismanaziz <mrismanaziz@gmail.com>
2021-10-01 14:54:07 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/adzan.py
+++ userbot/modules/adzan.py
@@ -5,9 +5,8 @@ import json
 
 import requests
 
-from userbot import CMD_HELP
 from userbot import CMD_HANDLER as cmd
-from userbot import bot
+from userbot import CMD_HELP, bot
 from userbot.events import man_cmd
 from userbot.modules.sql_helper.globals import gvarstatus
 from userbot.utils import edit_delete, edit_or_reply
