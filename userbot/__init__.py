73457bc0b38c4fd0be89ccc66820b52110f29d8c
nimascnay <91711163+nimascnay@users.noreply.github.com>
2021-10-05 17:41:18 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -191,7 +191,7 @@ ZIP_DOWNLOAD_DIRECTORY = os.environ.get("ZIP_DOWNLOAD_DIRECTORY", "./zips")
 BITLY_TOKEN = os.environ.get("BITLY_TOKEN", None)
 
 # Bot Name
-TERM_ALIAS = os.environ.get("TERM_ALIAS", "Ｉｃｅ-Userbot")
+TERM_ALIAS = os.environ.get("TERM_ALIAS", "Ice-Userbot")
 
 # Bot version
 BOT_VER = os.environ.get("BOT_VER", "5.5.0")
