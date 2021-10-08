971031586f21a9e3856de2db05c135268aba567b
skyzuu <88702146+Askarbot@users.noreply.github.com>
2021-10-08 16:50:30 +0700

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -35,19 +35,6 @@ async def typewriter(typew):
     await typew.edit("**Wa'alaikumsalam**")
 
 
-@register(outgoing=True, pattern=r"^\.ceca(?: |$)(.*)")
-async def typewriter(typew):
-    typew.pattern_match.group(1)
-    sleep(1.5)
-    await typew.edit("**Hy**")
-    sleep(1.5)
-    await typew.edit("**salken💅🏻**")
-    sleep(1.5)
-    await typew.edit("**princess ceca🦄**")
-    sleep(1.5)
-    await typew.edit("**call me ceca🐻**")
-
-
 CMD_HELP.update(
     {
         "salam": "**Plugin : **`salam`\
