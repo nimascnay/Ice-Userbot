65ec0294ed887a596f941ea241672ba583394dc6
skyzuu <88702146+Askarbot@users.noreply.github.com>
2021-10-08 17:02:13 +0700

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -29,7 +29,6 @@ async def typewriter(typew):
     await typew.edit("**Assalamualaikum...**")
 
 
-
 @register(outgoing=True, pattern=r"^\.l(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
