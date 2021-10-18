c2933e617f51893ccbbd4ed97955cfc0f4c31b91
Puutraaa <88121146+Puutraaa@users.noreply.github.com>
2021-10-18 09:56:46 +0800

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -23,7 +23,6 @@ from pathlib import Path
 from pylast import LastFMNetwork, md5
 from pySmartDL import SmartDL
 from pymongo import MongoClient
-from redis import StrictRedis
 from dotenv import load_dotenv
 from requests import get
 from telethon.sync import TelegramClient, custom, events
